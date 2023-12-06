f0c9cb9807f24d5746e5a71e11ede5c348ac9b06
Bolshov92 <bolshov92@gmail.com>
2023-12-06 22:22:49 +0000

    add task with hashMap and compare and upDate from Faker

--- src/main/java/com/prof/homework/HashMap/UserApp.java
+++ src/main/java/com/prof/homework/HashMap/UserApp.java
@@ -1,31 +1,34 @@
 package com.prof.homework.HashMap;
 
+import com.github.javafaker.Faker;
+
 import java.util.Comparator;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class UserApp {
 
+    private static final Faker FAKER = new Faker();
 
     public static void main(String[] args) {
 
         Map<User, String> ageSort = new TreeMap<>();
-        ageSort.put(new User("Alex", 15), "Info");
-        ageSort.put(new User("Slava", 20), "Info");
-        ageSort.put(new User("Vova", 30), "Info");
+        ageSort.put(new User(FAKER.name().firstName(), 15), "Info");
+        ageSort.put(new User(FAKER.name().firstName(), 20), "I");
+        ageSort.put(new User(FAKER.name().firstName(), 30), "o");
 
         for (Map.Entry<User, String> e : ageSort.entrySet()) {
-            System.out.println(e.getKey() + e.getValue());
+            System.out.println(e.getKey().getName()  + " " + e.getKey().getAge() + " "  + e.getValue());
 
         }
 
 
         Map<User, String> sort = new TreeMap<>(new UserComparator());
-        sort.put(new User("Alex", 15), "Info");
-        sort.put(new User("Wlava", 20), "Info");
-        sort.put(new User("Vova", 30), "Info");
-        sort.put(new User("Blava", 20), "Info");
-        sort.put(new User("Zova", 30), "Info");
+        sort.put(new User(FAKER.name().firstName(), 15), "Info");
+        sort.put(new User(FAKER.name().firstName(), 20), "Info");
+        sort.put(new User(FAKER.name().firstName(), 30), "Info");
+        sort.put(new User(FAKER.name().firstName(), 20), "Info");
+        sort.put(new User(FAKER.name().firstName(), 30), "Info");
         for(Map.Entry<User, String> e : sort.entrySet()){
             System.out.println(e.getKey().getName());
         }
