a598171eea1cb60f75d3f17a10e37b61feb8a5d5
Bolshov92 <bolshov92@gmail.com>
2024-02-14 18:05:01 +0000

    Add Class Check Valid NumberCard and Class REGEX for practice regular expressions

---
+++ src/main/java/com/prof/java/App.java
@@ -0,0 +1,36 @@
+package com.prof.java;
+
+public class App {
+
+    public static void main(String[] args) {
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Type your age -> ");
+//        int age = scanner.nextInt();
+//        if (age < 18) {
+//            System.out.println("You child");
+//        } else {
+//            System.out.println("Yo Adult");
+//        }
+//    }
+//        String cold = "на улице холодно";
+//        String warm = "на улице тепло";
+//        //напишите тут ваш код
+//        Scanner Scanner = new Scanner(System.in);
+//        System.out.println("Type  Curently Temperature : ");
+//        int temp = scanner.nextInt();
+//
+//        if (temp < 0) {
+//            System.out.println(cold);
+//        } else {
+//            System.out.println(warm);
+//        }
+        String a = "Hello";
+        String b ="hello";
+        String result = a.equalsIgnoreCase(b) ? "True" : "false";
+        System.out.println(result);
+    }
+}
+
+
+
